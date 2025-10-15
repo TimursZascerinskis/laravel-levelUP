661ca8d074711fa3bbc5dcb43e59437dfa7d454e
unknown <your@email.example>
2025-10-15 11:14:02 +0300

    factory un seed data

---
+++ database/factories/ProductFactory.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace Database\Factories;
+
+use App\Models\Product;
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+class ProductFactory extends Factory
+{
+    protected $model = Product::class; // saista factory ar Product modeli
+
+    public function definition()
+    {
+        return [
+            'name' => $this->faker->word(),
+            'quantity' => $this->faker->numberBetween(1, 100),
+            'description' => $this->faker->sentence(),
+        ];
+    }
+}
